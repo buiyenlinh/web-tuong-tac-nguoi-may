56582223440ab46cf276ec24941b358ef30dd397
buiyenlinh <buiyenlinh1406@gmail.com>
2021-08-31 14:26:29 +0700

    fix confict

--- ad_api.php
+++ ad_api.php
@@ -181,11 +181,8 @@ else if ($action == 'update-user') {
     if (empty($check)) {
         _error('Người dùng không tồn tại');
     }
-<<<<<<< HEAD
-    if ($check['role'] < $_SESSION['id']) {
-=======
+    
     if ($_SESSION['user']['vaitro'] >= $check['vaitro']) {
->>>>>>> master
         _error('Bạn không có quyền thay đổi tài khoản này!');
     }
 
