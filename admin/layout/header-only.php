a276c31d0dce8410b163f49c43e2de15d2512dfb
ThanhChanh-1420 <chanhuxhyt@gmail.com>
2021-10-01 20:11:57 +0700

    update

--- admin/layout/header-only.php
+++ admin/layout/header-only.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Animal</title>
+    <link rel="icon" type="image/png" href="https://www.pikpng.com/pngl/m/289-2895808_clipart-the-noble-big-transparent-gecko-silhouette-png.png">
     <link rel="stylesheet" href="<?php echo BASE?>bootstrap/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href=" <?php echo BASE ?>css/ad_style.css" >
